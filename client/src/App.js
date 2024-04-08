17af9defa065130b5f0e1fc3ba1fbe1e6ff01c6f
Maleesha Dinuranga <maleeshadmax@gmail.com>
2024-04-08 21:33:35 +0530

    Tailwind Setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold">Netflix clone</h1>
+    </div>
+  );
+}
+
+export default App;
