039552c1435871a6535fc639f94f0bf557a8f819
Maleesha Dinuranga <maleeshadmax@gmail.com>
2024-04-10 12:48:20 +0530

    production

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Home from "./components/Home";
 import SplitSection from "./components/SplitSection";
-import Image2 from "./components/views/Image2";
 import Footer from "./components/Footer";
 import FAQ from "./components/FAQ";
 
